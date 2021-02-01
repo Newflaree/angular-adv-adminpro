f254f337e0fe9ba8220beca9397dff9e0c7114e9
Newflare <camiloj.lopez79@gmail.com>
2021-02-01 16:16:26 -0300

    Login y Auth

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor( 
+    private usuarioService: UsuarioService,
+  ) { }
 
-  ngOnInit(): void {
-  }
+  logout() {
+    this.usuarioService.logout();
+  } 
+
 
 }
